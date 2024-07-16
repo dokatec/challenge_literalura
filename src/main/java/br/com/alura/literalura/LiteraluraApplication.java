2c065abe551453a95cb9bac7a2df9df4078d5f06
Jhonatan Pacheco <47837212+dokatec@users.noreply.github.com>
2024-07-16 18:00:13 -0300

    atualizando metodos do projeto

--- src/main/java/br/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/br/com/alura/literalura/LiteraluraApplication.java
@@ -5,10 +5,22 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.literalura.dto.BookDTO;
+import br.com.alura.literalura.model.Author;
+import br.com.alura.literalura.model.Book;
+import br.com.alura.literalura.repository.BookRepository;
+import br.com.alura.literalura.service.ConsumirAPI;
+
 import java.util.*;
 
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private ConsumirAPI consumirAPI;
+
+	@Autowired
+	private BookRepository bookRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
@@ -37,11 +49,11 @@ public class LiteraluraApplication implements CommandLineRunner {
 					break;
 
 				case 4:
-					findAuthorByName(scanner);
+					// findAuthorByName(scanner);
 					break;
 
 				case 5:
-					listAuthorsAliveByYear(scanner);
+					// listAuthorsAliveByYear(scanner);
 					break;
 
 				case 0:
@@ -69,28 +81,73 @@ public class LiteraluraApplication implements CommandLineRunner {
 	}
 
 	private void findBookByTitle(Scanner scanner) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'findBookByTitle'");
+		System.out.println("Digite o título do livro:");
+		String title = scanner.nextLine();
+
+		try {
+			List<BookDTO> books = consumirAPI.searchBooks(title);
+
+			if (books.isEmpty()) {
+				System.out.println("Nenhum livro encontrado");
+			} else {
+				for (int i = 0; i < books.size(); i++) {
+					System.out.println(i + " - " + books.get(i).title());
+				}
+
+				System.out.println("Digite o número do livro que deseja salvar:");
+				int index = scanner.nextInt();
+				scanner.nextLine();
+
+				Book book = new Book(books.get(index));
+				bookRepository.save(book);
+				System.out.println("Livro salvo com sucesso!");
+
+			}
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	private void listBooks() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'listBooks'");
+		List<Book> books = bookRepository.findAll();
+
+		if (books.isEmpty()) {
+			System.out.println("Nenhum livro salvo");
+
+		} else {
+			System.out.println("Lista de livros salvos: ");
+			for (Book book : books) {
+				System.out.println("- " + book.getTitle());
+			}
+		}
+
 	}
 
 	private void listAuthors() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'listAuthors'");
+		List<Author> authors = bookRepository.findAllAuthors();
+
+		if (authors.isEmpty()) {
+			System.out.println("Nenhum autor salvo");
+		} else {
+			System.out.println("Lista de autores salvos:");
+			for (Author author : authors) {
+				System.out.println("- " + author.getName());
+
+			}
+		}
 	}
 
-	private void findAuthorByName(Scanner scanner) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'findAuthorByName'");
-	}
+	// private void findAuthorByName(Scanner scanner) {
+	// // TODO Auto-generated method stub
+	// throw new UnsupportedOperationException("Unimplemented method
+	// 'findAuthorByName'");
+	// }
 
-	private void listAuthorsAliveByYear(Scanner scanner) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'listAuthorsAliveByYear'");
-	}
+	// private void listAuthorsAliveByYear(Scanner scanner) {
+	// // TODO Auto-generated method stub
+	// throw new UnsupportedOperationException("Unimplemented method
+	// 'listAuthorsAliveByYear'");
+	// }
 
 }
--- src/main/java/br/com/alura/literalura/model/Author.java
+++ src/main/java/br/com/alura/literalura/model/Author.java
@@ -6,9 +6,6 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.hibernate.annotations.ManyToAny;
-import org.hibernate.engine.internal.Cascade;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
